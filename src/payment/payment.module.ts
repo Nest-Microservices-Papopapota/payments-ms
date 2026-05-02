cca454b36af804e84c4e3128b840175d982bf5cd
Daniel Olivares <76667767+papopapota@users.noreply.github.com>
2026-05-02 17:32:07 -0500

    se agrego el nats module y se importo el nats module en payment module

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { NatsModule } from 'src/transport/nats.module';
 
 @Module({
   controllers: [PaymentController],
   providers: [PaymentService],
+  imports: [NatsModule],
 })
 export class PaymentModule {}
